f4c4a4a91e075e3499f07565e80531a325d1bcda
jim schyvens <patron@MacBook-Pro-van-Jim.local>
2023-05-23 12:46:18 +0200

    added login

--- src/main/java/com/patron/App.java
+++ src/main/java/com/patron/App.java
@@ -1,6 +1,8 @@
 package com.patron;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App 
@@ -11,6 +13,17 @@ public class App
         WebDriver webDriver = new ChromeDriver();
 
         webDriver.get(path);
+
+        WebElement formAuthenticationLink = webDriver.findElement(By.linkText("Form Authentication"));
+        formAuthenticationLink.click();
+
+        WebElement username = webDriver.findElement(By.id("username"));
+        WebElement password = webDriver.findElement(By.id("password"));
+        WebElement login = webDriver.findElement(By.cssSelector("button[type='submit']"));
+
+        username.sendKeys("tomsmith");
+        password.sendKeys("SuperSecretPassword!");
+        login.click();
 
     }
 }
